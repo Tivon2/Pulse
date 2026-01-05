419695a1381acbc6f724ae5288dc27f31b760fec
Oluwafemi Azobu <Oluwafemiazobu@trentu.ca>
2026-01-05 18:29:41 -0500

    Initial Commit

---
+++ Pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pulse
+//
+//  Created by Azobu daniel on 2026-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Azobu daniel on 2026-01-05.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
