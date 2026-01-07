cff533f698c473440cedeb1933b7ca86999c6d51
Oluwafemi Azobu <Oluwafemiazobu@trentu.ca>
2026-01-06 22:46:28 -0500

    Updated sleepEntry.swift

--- Models/SleepEntry.swift
+++ Models/SleepEntry.swift
@@ -1,26 +1,44 @@
-import foundation
+import Foundation
 
-struct SleepEntry: Identifiable, codable {
-    let id: UUID ()
+enum SleepQuality: Int, Codable {
+    case poor = 1
+    case moderate = 2
+    case good = 3
+    case excellent = 4
+}
+
+struct SleepEntry: Identifiable, Codable {
+    let id: UUID
     let date: Date
     let bedtime: Date
     let wakeTime: Date
     let duration: TimeInterval
+    let quality: SleepQuality
     let sleepScore: Int
-    
-    init (date: Date, bedtime: Date, wakeTime; Date){
+
+    init(date: Date = Date(), bedtime: Date, wakeTime: Date) {
         self.id = UUID()
         self.date = date
         self.bedtime = bedtime
         self.wakeTime = wakeTime
         self.duration = wakeTime.timeIntervalSince(bedtime)
-        self.sleepScore = SleepEntry.calculateScore(duration: duration)
+        self.quality = SleepEntry.calculateQuality(duration: duration)
+        self.sleepScore = quality.rawValue
     }
-    
-    private static func calculateScore(duration: TimeInterval) -> Init {
+
+    // MARK: Sleep Quality Calculation
+    private static func calculateQuality(duration: TimeInterval) -> SleepQuality {
         let hours = duration / 3600
+
         switch hours {
-        case 7..9: return 
+        case 7..<9:
+            return .excellent
+        case 6..<7, 9..<10:
+            return .good
+        case 5..<6, 10..<11:
+            return .moderate
+        default:
+            return .poor
         }
     }
 }
